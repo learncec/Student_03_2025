13ed96f10af73e7e3e407de298dae39a5e32ac75
CEC <learnmorececc1@gmail.com>
2025-07-10 10:54:19 +0200

    Initial sharing of project

---
+++ Studenr.java
@@ -0,0 +1,36 @@
+public class Studenr
+{
+    private String  name;
+    private int     alter;
+    private boolean matura;
+    
+    public void setName(String newName)
+    {
+        name = newName;
+    }
+    
+    public void setAlter(int newAlter)
+    {
+        alter = newAlter;
+    }
+    
+    public void setMatura(boolean newMatura)
+    {
+        matura = newMatura;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public int getAlter()
+    {
+        return alter;
+    }
+    
+    public boolean getMatura()
+    {
+        return matura;
+    }
+}
